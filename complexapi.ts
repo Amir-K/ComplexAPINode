cca96212138701e3cd8f31728a8347d3bb7b0917
Amir Kodro <kodroamir@gmail.com>
2025-08-03 21:03:42 -0700

    fix port

--- complexapi.ts
+++ complexapi.ts
@@ -182,7 +182,7 @@ async function startServer() {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-  const PORT = process.env.PORT || 9000;
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
   });
